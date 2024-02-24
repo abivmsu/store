f1c6c8d63febecc8b495e869d399b23d1615d903
abivmsul <98397583+abivmsul@users.noreply.github.com>
2024-02-25 00:12:51 +0300

    Initial commit

--- andelus/settings.py
+++ andelus/settings.py
@@ -147,6 +147,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Media files (user-uploaded content)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
